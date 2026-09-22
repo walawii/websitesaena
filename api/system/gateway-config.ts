3ebbbe9c9ec8e8943115bafe4931a4c44a9eecf2
walawii <52245839+walawii@users.noreply.github.com>
2026-09-22 15:24:32 +0700

    Add standalone gateway config endpoint

---
+++ api/system/gateway-config.ts
@@ -0,0 +1,16 @@
+import { META_DATASET_ID } from '../../server/metaCapiService';
+
+export default function handler(_req: any, res: any) {
+  return res.status(200).json({
+    doku: {
+      configured: !!(process.env.DOKU_CLIENT_ID && process.env.DOKU_SECRET_KEY)
+    },
+    mengantar: {
+      configured: !!process.env.MENGANTAR_API_KEY
+    },
+    meta: {
+      configured: !!process.env.META_CAPI_ACCESS_TOKEN,
+      datasetId: META_DATASET_ID
+    }
+  });
+}
